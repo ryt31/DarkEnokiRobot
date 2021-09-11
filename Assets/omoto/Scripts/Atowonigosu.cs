618276bdf5946abdf2f12eaa1f0240b319df8453
Endered <yy56ga10ve@gmail.com>
2021-09-11 18:52:57 +0900

    fix atowonigosu for player class

--- Assets/omoto/Scripts/Atowonigosu.cs
+++ Assets/omoto/Scripts/Atowonigosu.cs
@@ -16,6 +16,10 @@ public class Atowonigosu : MonoBehaviour
     private float NigosaretaRadius = 1f; // オブジェクトが濁されるために必要な飛翔距離
     [SerializeField]
     private float MissingRadius = 100f; // MissingRadius以上の距離を飛んだオブジェクトの追跡をやめる。
+    [SerializeField]
+    private Transform explosionCenter; // 爆発の中心
+    [SerializeField]
+    private Player player; // Player Componentへの参照
 
     public bool CanNigosu; // Nigosu関数を実行してもよいか
     private int reminderObject; // Nigosu関数で吹き取んでいるオブジェクトの個数
@@ -30,6 +34,13 @@ public class Atowonigosu : MonoBehaviour
     {
         // Nigosu関数で吹き飛ばすかどうかの判定をする関数
         return Mathf.Pow(target.GetComponent<Rigidbody2D>().mass, 2f) < power;
+    }
+
+    private void addPlayerSize(float add){
+        player.PlayerSize += add;
+    }
+    private void decreaseNigoseru(){
+        --player.Nigoseru;
     }
 
     private IEnumerator TrackObject(GameObject target)
@@ -69,18 +80,21 @@ public class Atowonigosu : MonoBehaviour
     {
         // オブジェクトを吹き飛ばす関数。
         CanNigosu = false;
-        yield return null;
-        Collider2D[] objects = Physics2D.OverlapCircleAll(this.transform.position, radius);
+        decreaseNigoseru();
 
+        yield return null;
+
+        Vector3 center = explosionCenter.position;
+        Collider2D[] objects = Physics2D.OverlapCircleAll(center, radius);
         reminderObject = 0;
 
         foreach (var i in objects)
         {
-            float dist = Vector2.Distance(this.transform.position, i.transform.position);
+            float dist = Vector2.Distance(center, i.transform.position);
             Rigidbody2D rigid = i.GetComponent<Rigidbody2D>();
             if (rigid == null) continue;
             if (!IsNigosuable(i.gameObject, power)) continue;
-            Vector2 v = i.transform.position - this.transform.position;
+            Vector2 v = i.transform.position - center;
             Vector2 add = v.normalized * power * 1 / v.sqrMagnitude;
             rigid.AddForce(add);
             StartCoroutine(TrackObject(i.gameObject));
@@ -91,6 +105,7 @@ public class Atowonigosu : MonoBehaviour
             Debug.Log(reminderObject);
         } while (reminderObject > 0);
         float score = scoringNigosu(Nigosareta);
+        addPlayerSize(score);
         Debug.Log("You're score: " + score.ToString());
         Nigosareta.Clear();
         CanNigosu = true;
@@ -121,5 +136,14 @@ public class Atowonigosu : MonoBehaviour
     {
         Nigosareta = new List<(GameObject, Vector2)>();
         CanNigosu = true;
+        if(explosionCenter == null){
+            explosionCenter = this.transform;
+        }
+        if(player == null){
+            player = this.GetComponent<Player>();
+            if(player == null){
+                Debug.Log("Start:ERROR there are no Player Component");
+            }
+        }
     }
 }
